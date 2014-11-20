5dfaa5563bbdd7ad54f401df74e7dd2e5da184ba
guipecea@gmail.com <guipecea@gmail.com@0328f62e-7beb-258c-4aac-f44a843d1f2f>
2014-11-20 20:19:29 +0000

    Req Especial 1.3::  PRONTO
    Chequeen el mail que les envio, tienen que hacer algunos ajustes.

--- trunk/ClienteWebPrueba/src/java/Servlets/ICordenCompra.java
+++ trunk/ClienteWebPrueba/src/java/Servlets/ICordenCompra.java
@@ -5,6 +5,10 @@
  */
 package Servlets;
 
+import java.io.FileInputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Properties;
 import webService.OrdenCompra;
 /**
  *
@@ -12,50 +16,67 @@ import webService.OrdenCompra;
  */
 public class ICordenCompra {
 
-    public void addLineaToOrdenCompra(int arg0, int arg1) {
-        webService.WsIordenCompraService service = new webService.WsIordenCompraService();
+    private URL getParametros() throws MalformedURLException{
+        try{
+        Properties preferences = new Properties();
+        FileInputStream in = new FileInputStream(System.getProperty("user.home")+"/configWebServices.ini");
+        preferences.load(in);
+        String host = preferences.getProperty("Host");
+        String port = preferences.getProperty("Port");
+        URL ubicacion= new URL("http://"+host+":"+port+"/iordencompra");
+        return ubicacion;
+        }
+            catch(Exception ex){
+              //URL ubicacion= new URL("http://localhost:8280/iordencompra");
+              //return ubicacion;
+                return null;
+            }
+    }
+    
+    public void addLineaToOrdenCompra(int arg0, int arg1) throws MalformedURLException {
+        webService.WsIordenCompraService service = new webService.WsIordenCompraService(this.getParametros());
         webService.WsIordenCompra port = service.getWsIordenCompraPort();
         port.addLineaToOrdenCompra(arg0, arg1);
     }
 
-    public void createOrdenCompra(int arg0, java.lang.String arg1, float arg2, java.lang.String arg3) {
-        webService.WsIordenCompraService service = new webService.WsIordenCompraService();
+    public void createOrdenCompra(int arg0, java.lang.String arg1, float arg2, java.lang.String arg3) throws MalformedURLException {
+        webService.WsIordenCompraService service = new webService.WsIordenCompraService(this.getParametros());
         webService.WsIordenCompra port = service.getWsIordenCompraPort();
         port.createOrdenCompra(arg0, arg1, arg2, arg3);
     }
 
-    public int createOrdenCompra2(java.lang.String arg0, float arg1, java.lang.String arg2, webService.Estado arg3) {
-        webService.WsIordenCompraService service = new webService.WsIordenCompraService();
+    public int createOrdenCompra2(java.lang.String arg0, float arg1, java.lang.String arg2, webService.Estado arg3) throws MalformedURLException {
+        webService.WsIordenCompraService service = new webService.WsIordenCompraService(this.getParametros());
         webService.WsIordenCompra port = service.getWsIordenCompraPort();
         return port.createOrdenCompra2(arg0, arg1, arg2, arg3);
     }
 
-    public void editOrdenCompra2(int arg0, java.lang.String arg1, float arg2, java.lang.String arg3, webService.Estado arg4, java.lang.String arg5, java.lang.String arg6) {
-        webService.WsIordenCompraService service = new webService.WsIordenCompraService();
+    public void editOrdenCompra2(int arg0, java.lang.String arg1, float arg2, java.lang.String arg3, webService.Estado arg4, java.lang.String arg5, java.lang.String arg6) throws MalformedURLException {
+        webService.WsIordenCompraService service = new webService.WsIordenCompraService(this.getParametros());
         webService.WsIordenCompra port = service.getWsIordenCompraPort();
         port.editOrdenCompra2(arg0, arg1, arg2, arg3, arg4, arg5, arg6);
     }
 
-    public void eliminarOrdenCompra(int arg0) {
-        webService.WsIordenCompraService service = new webService.WsIordenCompraService();
+    public void eliminarOrdenCompra(int arg0) throws MalformedURLException {
+        webService.WsIordenCompraService service = new webService.WsIordenCompraService(this.getParametros());
         webService.WsIordenCompra port = service.getWsIordenCompraPort();
         port.eliminarOrdenCompra(arg0);
     }
 
-    public OrdenCompra findOrdenCompra(int arg0) {
-        webService.WsIordenCompraService service = new webService.WsIordenCompraService();
+    public OrdenCompra findOrdenCompra(int arg0) throws MalformedURLException {
+        webService.WsIordenCompraService service = new webService.WsIordenCompraService(this.getParametros());
         webService.WsIordenCompra port = service.getWsIordenCompraPort();
         return port.findOrdenCompra(arg0);
     }
 
-    public java.util.List<webService.OrdenCompra> findOrdenCompraEntities() {
-        webService.WsIordenCompraService service = new webService.WsIordenCompraService();
+    public java.util.List<webService.OrdenCompra> findOrdenCompraEntities() throws MalformedURLException {
+        webService.WsIordenCompraService service = new webService.WsIordenCompraService(this.getParametros());
         webService.WsIordenCompra port = service.getWsIordenCompraPort();
         return port.findOrdenCompraEntities();
     }
 
-    public java.util.List<webService.LineaOrden> getListadoLineasXord(int arg0) {
-        webService.WsIordenCompraService service = new webService.WsIordenCompraService();
+    public java.util.List<webService.LineaOrden> getListadoLineasXord(int arg0) throws MalformedURLException {
+        webService.WsIordenCompraService service = new webService.WsIordenCompraService(this.getParametros());
         webService.WsIordenCompra port = service.getWsIordenCompraPort();
         return port.getListadoLineasXord(arg0);
     }
