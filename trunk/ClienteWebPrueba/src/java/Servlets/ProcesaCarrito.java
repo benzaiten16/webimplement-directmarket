88a8eba88c52b39e7dea93b9c3276191c9c7325f
guipecea@gmail.com <guipecea@gmail.com@0328f62e-7beb-258c-4aac-f44a843d1f2f>
2014-11-20 22:26:50 +0000

    Confirma Orden Compra Agregado y probado.

--- trunk/ClienteWebPrueba/src/java/Servlets/ProcesaCarrito.java
+++ trunk/ClienteWebPrueba/src/java/Servlets/ProcesaCarrito.java
@@ -6,11 +6,13 @@
 package Servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import webService.Estado;
 
 /**
  *
@@ -27,20 +29,64 @@ public class ProcesaCarrito extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try {
-			String usr = Login.getUsuarioLogueado(request);
-			request.setAttribute("usuario", usr);
-			request.getRequestDispatcher("/WEB-INF/errorPages/PaginaEnConstruccion.jsp").forward(request, response);
-		
-        }catch(Exception ex){
-			// no existe el usuario, se trata como deslogueado
-			request.getSession().setAttribute("estado_sesion", EstadoSesion.NO_LOGIN);
-			request.getRequestDispatcher("/WEB-INF/errorPages/InicieSesion.jsp").forward(request, response);
-		}
-	}
+protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    throws ServletException, IOException {
+    response.setContentType("text/html;charset=UTF-8");
+try {
+    //String usr = Login.getUsuarioLogueado(request);
+    String usr="GUIPECEA";
+    System.out.println(request.getSession().getAttribute("usuario_logueado").toString()+" <---ES EL USUARIO");
+    //request.setAttribute("usuario_logueado", usr);
+    //request.getRequestDispatcher("/WEB-INF/errorPages/PaginaEnConstruccion.jsp").forward(request, response);
+        
+    //OBTENGO ARRAYS DE AQUELLOS SELECCIONADOS
+    float total=0;
+    
+    IClineaOrden ICLORD= new IClineaOrden();
+    ICordenCompra ICORDC = new ICordenCompra();
+    
+    String [] numReferencia = request.getParameterValues("numRef");
+    String [] precioCompra = request.getParameterValues("precio");
+    String [] cantidad = request.getParameterValues("Cantidad");
+    
+    Date ahora = new Date();
+    SimpleDateFormat formato_fecha= new SimpleDateFormat("dd-MM-yyyy-mm");
+    String fecha = formato_fecha.format(ahora);
+    
+    //PARA SACAR EL TOTAL DE LA ORDEN
+    for(int i=0; i<numReferencia.length; i++){
+        
+        int cantActual = Integer.parseInt(cantidad[i]);
+    
+        float precioCompraAct = Float.parseFloat(precioCompra[i]);
+    
+        //total+=precioCompraAct*cantActual;
+        total=total+(precioCompraAct*cantActual);
+    }
+    
+    int numOrdenCom = ICORDC.createOrdenCompra2(fecha, total, usr , Estado.RECIBIDA);
+    
+    //System.out.println("SEGUNDO FOR LUEGO DE CREAR LA ORDEN");
+    for(int i=0; i<numReferencia.length; i++){
+        
+        int prodActual = Integer.parseInt(numReferencia[i]);
+        int cantActual = Integer.parseInt(cantidad[i]);
+        float precioCompraAct = Float.parseFloat(precioCompra[i]);
+        total+=precioCompraAct*cantActual;
+        int numLinea = ICLORD.intcreateLineaOrden(cantActual, precioCompraAct, prodActual);
+        
+        ICORDC.addLineaToOrdenCompra(numOrdenCom, numLinea);
+        
+    }                    
+    
+    request.getRequestDispatcher("/WEB-INF/errorPages/OrdenCompraOk.jsp").forward(request, response);                  
+
+    }catch(Exception ex){
+    // no existe el usuario, se trata como deslogueado
+        request.getSession().setAttribute("estado_sesion", EstadoSesion.NO_LOGIN);
+        request.getRequestDispatcher("/WEB-INF/errorPages/InicieSesion.jsp").forward(request, response);
+    }
+}
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
